a464a3882c85a9f2481ae43fe9aed9fd20bc67ba
olvera93 <olvera9302@outlook.com>
2025-10-20 13:02:15 -0600

    [ADD] Skeleton

---
+++ AllDoneComplete/AllDoneCompleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllDoneCompleteApp.swift
+//  AllDoneComplete
+//
+//  Created by Gonzalo Olvera on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AllDoneCompleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllDoneComplete/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AllDoneComplete
+//
+//  Created by Gonzalo Olvera on 20/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
