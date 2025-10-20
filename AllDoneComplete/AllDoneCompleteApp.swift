2d31ae0a688cee8ab67779bf3723df298dfa09d5
olvera93 <olvera9302@outlook.com>
2025-10-20 14:40:52 -0600

    [ADD] Firebase importer

--- AllDoneComplete/AllDoneCompleteApp.swift
+++ AllDoneComplete/AllDoneCompleteApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct AllDoneCompleteApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppStartingView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
--- AllDoneComplete/Core/AppStartingView.swift
+++ AllDoneComplete/Core/AppStartingView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  AppStartingView.swift
 //  AllDoneComplete
 //
 //  Created by Gonzalo Olvera on 20/10/25.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct AppStartingView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    AppStartingView()
 }
